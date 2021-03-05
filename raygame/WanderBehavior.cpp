46873cc99078cc23daae3f4a271719cd9b91fb06
Josiah Hartley <josiah.t.hartley@gmail.com>
2021-03-05 15:18:43 -0600

    Converted some sudeo code for wander calculate

--- raygame/WanderBehavior.cpp
+++ raygame/WanderBehavior.cpp
@@ -19,12 +19,17 @@ WanderBehavior::WanderBehavior(MathLibrary::Vector2 target, float wanderForce, f
 
 MathLibrary::Vector2 WanderBehavior::calculateForce(Agent* agent)
 {
+	//create a random degree between 0 and 360
+	float theta = rand() / RAND_MAX/360;
 	//Start with Random Target somewhere congruent with the radius of this agent
-	m_target;
+	m_target = MathLibrary::Vector2((cos(theta * m_radius)), (sin(theta * m_radius)));
 	//Add a random vector to the the Random Target with a magnitude specified by a jitter amount
+	theta = rand() / RAND_MAX / 360;
+	m_target = MathLibrary::Vector2(m_target.x + cos(theta * m_jitter), m_target.y + sin(theta * m_jitter));
 	//Bring the Random Target to be congruent with the perimeter of the radius by normalizing and scaling by radius
 	//Add the agents current vector multiplied by a random distance to the random target
 	//Return the random target
+	return MathLibrary::Vector2();
 }
 
 void WanderBehavior::update(Agent* agent, float deltaTime)
